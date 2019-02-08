429748107bd31804ec989dab41536249de5c0478
jeffreyg-sh <jeffrey@steelhouse.com>
2019-02-07 17:40:19 -0800

    Added basic file logging

--- src/main/java/com/steelhouse/audience/RunStorage.java
+++ src/main/java/com/steelhouse/audience/RunStorage.java
@@ -1,14 +1,15 @@
 package com.steelhouse.audience;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 
 
 @Controller
+@Slf4j
 public class RunStorage {
 
   @Resource
